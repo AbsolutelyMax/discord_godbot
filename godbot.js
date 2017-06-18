3c51af4e64f2cbf74db7147db41dd12a51363937
Birdboat <birdserver1@gmail.com>
2017-06-18 18:11:44 -0500

    yeet frick js

--- godbot.js
+++ godbot.js
@@ -5,9 +5,33 @@ const config = require("./config.json");
 const fs = require("fs");
 var commands = [];
 
-function getEmojiFromString(emoji)
+function getCommands() { return commands; }
+
+randomValue = function(min, max) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+eightBall = function(arg) {
+    var rNum = randomValue(1, 5);
+
+    if(rNum === 1) { return "No" } else
+    if(rNum === 2) { return "Yes, " + arg} else
+    if(rNum === 3) { return "Maybe" } else
+    if(rNum === 4) { return arg + " I think so" } else
+    if(rNum === 5) { return "I think not" } else
+    {
+        return "error";
+    }
+}
+
+function getEmojiFromString(emojiname)
 {
-  return "<:" + emoji + ":";
+  client.guilds.first().emojis.forEach(function(emoji) 
+  {
+    if (emoji.name === emojiname) return "<:" + emoji.identifier + ">";
+  });
+
+  return "";
 }
 
 client.on("ready", () => {
@@ -16,8 +40,18 @@ client.on("ready", () => {
   };
   
   commands[getEmojiFromString("therose")] = function(msg) {
-    casting.cast(Discord.Message, msg).channel.send(":therose:");
+    casting.cast(Discord.Message, msg).channel.send("bruh !!");
   }
+
+  commands["8ball"] = function(msg) {
+    let ans = "";
+    let postMsg = msg.content.slice(7);
+    ans = eightBall(postMsg);
+    casting.cast(Discord.Message, msg).channel.send(ans);
+  };
+
+  console.log(getEmojiFromString("therose"));
+
   console.log("god bot is present");
 });
 
