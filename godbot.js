e70af9edbb6b763c6a4bd3c8ec47a0c19f4f1cd3
Birdboat <birdserver1@gmail.com>
2017-06-18 16:53:36 -0500

    Commands stuff and casting

--- godbot.js
+++ godbot.js
@@ -5,18 +5,28 @@ const config = require("./config.json");
 const fs = require("fs");
 var commands = [];
 
+function getEmojiFromString(emoji)
+{
+  return "<:" + emoji + ":";
+}
+
 client.on("ready", () => {
-  console.log("god bot is present");
   commands["africa"] = function(msg) { 
     casting.cast(Discord.Message, msg).channel.send("https://www.youtube.com/watch?v=FTQbiNvZqaY");
-   };
+  };
+  
+  commands[getEmojiFromString("therose")] = function(msg) {
+    casting.cast(Discord.Message, msg).channel.send(":therose:");
+  }
+  console.log("god bot is present");
 });
 
 client.on("message", msg => {
 	let prefix = config.prefix;
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
   //message.author.id !== config.ownerID
-  
+  console.log(msg.content);
+
   for (var command in commands)
   {
     if (msg.content.startsWith(prefix + command)) commands[command](msg);
