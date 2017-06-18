1bb5eb1aae179f1a3b56b5d338ce7bdaa9daf299
Birdboat <birdserver1@gmail.com>
2017-06-18 15:39:55 -0500

    Added casting and commands array

--- godbot.js
+++ godbot.js
@@ -1,30 +1,26 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const casting = require('casting');
 const config = require("./config.json");
 const fs = require("fs");
+var commands = [];
 
 client.on("ready", () => {
   console.log("god bot is present");
+  commands["africa"] = function(msg) { 
+    casting.cast(Discord.Message, msg).channel.send("https://www.youtube.com/watch?v=FTQbiNvZqaY");
+   };
 });
 
 client.on("message", msg => {
 	let prefix = config.prefix;
-
-    if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-    //message.author.id !== config.ownerID
-    
-    if (msg.content.startsWith(prefix + "help")) {
-		msg.channel.send("COMMANDS: africa");
-	} else
-
-    if (msg.content.startsWith(prefix + "africa")) {
-		msg.channel.send("https://www.youtube.com/watch?v=FTQbiNvZqaY");
-	} 
-
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+  //message.author.id !== config.ownerID
+  
+  for (var command in commands)
+  {
+    if (msg.content.startsWith(prefix + command)) commands[command](msg);
+  }
 });
-
-ask = function() {
-
-}
 
 client.login(config.token);
