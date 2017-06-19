bbaf37a93568631b8b6df3f5498dc829a7488595
Birdboat <birdserver1@gmail.com>
2017-06-18 19:14:50 -0500

    Added ytdl and Opus dependencies and playing capabilities

--- godbot.js
+++ godbot.js
@@ -2,8 +2,12 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const casting = require('casting');
 const config = require("./config.json");
+const yt = require('ytdl-core');
 const fs = require("fs");
 var commands = [];
+
+var currentStream;
+var queue = [];
 
 function getCommands() { return commands; }
 
@@ -35,22 +39,41 @@ function getEmojiFromString(emojiname)
 }
 
 client.on("ready", () => {
-  commands["africa"] = function(msg) { 
+  client.voiceConnections
+  commands["africa"] = function(msg, str) { 
+    console.log(str);
     casting.cast(Discord.Message, msg).channel.send("https://www.youtube.com/watch?v=FTQbiNvZqaY");
   };
   
+  /*
   commands[getEmojiFromString("therose")] = function(msg) {
     casting.cast(Discord.Message, msg).channel.send("bruh !!");
-  }
+  }*/
 
-  commands["8ball"] = function(msg) {
+  commands["8ball"] = function(msg, str) {
     let ans = "";
     let postMsg = msg.content.slice(7);
     ans = eightBall(postMsg);
     casting.cast(Discord.Message, msg).channel.send(ans);
   };
 
-  console.log(getEmojiFromString("therose"));
+  commands["ytplay"] = function(oldmsg, str)
+  {
+    var msg = casting.cast(Discord.Message, oldmsg);
+    if (msg.member.voiceChannel == null) return;
+    msg.member.voiceChannel.join().then(connection => 
+    {
+      const stream = yt(str, {filter: "audioonly"});
+      connection.playStream(stream);
+    }).catch(console.error);
+  }
+
+
+  commands["stop"] = function(oldmsg, str) 
+  {
+    var msg = casting.cast(Discord.Message, msg);
+    client.voiceConnections.first().st
+  }
 
   console.log("god bot is present");
 });
@@ -60,10 +83,9 @@ client.on("message", msg => {
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
   //message.author.id !== config.ownerID
   console.log(msg.content);
-
   for (var command in commands)
   {
-    if (msg.content.startsWith(prefix + command)) commands[command](msg);
+    if (msg.content.startsWith(prefix + command)) commands[command](msg, msg.content.substring(prefix.length + command.length + 1));
   }
 });
 
